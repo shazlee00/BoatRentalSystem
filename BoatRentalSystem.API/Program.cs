c852dc4dcbda0206802c2fe536fd7b67f4b976da
shazlee00 <melshazly56@gmail.com>
2024-08-25 15:34:53 +0300

    Loggin&Auth

--- BoatRentalSystem.API/Program.cs
+++ BoatRentalSystem.API/Program.cs
@@ -3,7 +3,16 @@ using BoatRentalSystem.Application;
 using BoatRentalSystem.Core.Interfaces;
 using BoatRentalSystem.Infrastructure;
 using BoatRentalSystem.Infrastructure.Repositories;
+using BoatSystem.Application;
+using BoatSystem.Core.Interfaces;
+using BoatSystem.Core.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +22,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.SwaggerDoc("user", new OpenApiInfo { Title = "User Api", Version = "v1" });
+//    options.SwaggerDoc("admin", new OpenApiInfo { Title = "Admin Api", Version = "v1" });
+//});
+
 
 builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
@@ -30,6 +45,54 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+
+builder.Host.UseSerilog();
+
+
+Log.Information("Starting up");
+
+
+builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+   .AddJwtBearer(o =>
+   {
+       o.RequireHttpsMetadata = false;
+       o.SaveToken = false;
+       o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+       {
+           ValidateIssuerSigningKey = true,
+           ValidateIssuer = true,
+           ValidateAudience = true,
+           ValidateLifetime = true,
+           ValidIssuer = builder.Configuration["JWT:Issuer"],
+           ValidAudience = builder.Configuration["JWT:Audience"],
+           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]
+           ))
+       };
+   });
+
+
+
+
+
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -38,10 +101,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+        
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- BoatRentalSystem.Infrastructure/ApplicationDbContext.cs
+++ BoatRentalSystem.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using BoatRentalSystem.Core.Entities;
+using BoatSystem.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace BoatRentalSystem.Infrastructure
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
@@ -28,6 +31,37 @@ namespace BoatRentalSystem.Infrastructure
             modelBuilder.ApplyConfiguration(new CountryConfiguration());
             modelBuilder.ApplyConfiguration(new PackageConfiguration());
             modelBuilder.ApplyConfiguration(new AdditionConfiguration());
+
+
+
+
+
+            modelBuilder.Entity<IdentityRole>(entity =>
+            {
+                entity.HasData(new IdentityRole
+                {
+                    Id = "7bdb9275-8cd4-4d86-bea6-bbdb5125e28a",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+
+                });
+
+
+                entity.HasData(new IdentityRole
+                {
+                    Id = "f117b498-2e53-4686-86dc-d3c13072850e",
+                    Name = "User",
+                    NormalizedName = "USER"
+
+                });
+            });
+
+
+
+
+
+
+
         }
 
 
